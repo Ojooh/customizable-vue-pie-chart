c5ca22a95e8240be64597db50c78718264c89368
Ojooh <42836583+Ojooh@users.noreply.github.com>
2024-10-01 11:31:00 +0000

    setting up the app properly

--- updated/customizable-vue-svg-pie-chart/src/App.vue
+++ updated/customizable-vue-svg-pie-chart/src/App.vue
@@ -1,17 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Customizable Vue.js Pie Chart</h1>
+    
+    <pie-chart 
+      :data="pieChartData" 
+      :segmentColors="segmentColors" 
+      :textSize="textSize"
+      :textColor="textColor"
+      :onSegmentClick="onSegmentClick"
+      :large="isLarge"
+      :showLegendText="true"
+      legendText="Sales Distribution"
+      legendPosition="top"
+      :showSegmentList="showSegmentList"
+    ></pie-chart>
+
+    <!-- Controls for adjusting chart -->
+    <div class="controls">
+      <label>
+        Text Size: 
+        <input type="range" v-model="textSize" min="1" max="10">
+      </label>
+      <label>
+        Text Color: 
+        <input type="color" v-model="textColor">
+      </label>
+      <label>
+        Large Size: 
+        <input type="checkbox" v-model="isLarge">
+      </label>
+      <label>
+        Show Segment List:
+        <input type="checkbox" v-model="showSegmentList">
+      </label>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PieChart from './components/PieChart.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PieChart,
+  },
+  data() {
+    return {
+      pieChartData: [
+        { label: "Segment 1", percentage: 40, link: "#" },
+        { label: "Segment 2", percentage: 30, link: "#" },
+        { label: "Segment 3", percentage: 20, link: "#" },
+        { label: "Segment 4", percentage: 10, link: "#" },
+      ],
+      segmentColors: ["#ff6384", "#36a2eb", "#cc65fe", "#ffce56"],
+      textSize: 4,
+      textColor: "#1F1D1D",
+      isLarge: false,
+      showSegmentList: true, // Optional segment list display
+    };
+  },
+  methods: {
+    onSegmentClick(segment) {
+      alert(`Clicked on segment: ${segment.label}`);
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +76,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.controls {
+  margin-top: 20px;
 }
 </style>
