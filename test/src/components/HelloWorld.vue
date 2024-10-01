e11cb7b43269e8d67ab14859610f7fd95b080a16
Ojooh <42836583+Ojooh@users.noreply.github.com>
2024-10-01 13:40:24 +0000

    FINAL VERSION READY

---
+++ test/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <PieChart :data="chartData" />
+  </div>
+</template>
+
+
+<script>
+import PieChart from 'customizable-vue-svg-pie-chart';
+
+export default {
+  components: {
+    PieChart,
+  },
+  data() {
+    return {
+      chartData: [
+        { label: "Segment 1", percentage: 40, color: "#ff6384" },
+        { label: "Segment 2", percentage: 30, color: "#36a2eb" },
+        { label: "Segment 3", percentage: 20, color: "#cc65fe" },
+        { label: "Segment 4", percentage: 10, color: "#ffce56" },
+      ]
+    };
+  }
+};
+
+</script>
+
